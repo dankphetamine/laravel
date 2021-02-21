3af77d7115a5cf7d86b5c2eb951bb656ca191de4
Storm K <asge0907@easv365.dk>
2021-02-21 22:36:19 +0100

    added form to add product

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,9 +2,8 @@
 
 @section('content')
 
-<h1>Create a product </h1>
 <div class="row">
-  <div class="col-12 margin-tb">
+  <div class="col">
      <div class="pull-left">
       <a class="btn btn-outline-secondary" href="{{route('products.index')}}">Go back</a>
     </div>
@@ -16,6 +15,19 @@
   </div>
 </div>
 
+<form action="{{route('products.store')}}" method="POST">
+  @csrf
+<div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" class="form-control" id="name" placeholder="Enter name">
+  </div>
+  <div class="form-group">
+    <label for="description">Password</label>
+    <input type="text" class="form-control" id="description" placeholder="Enter description">
+  </div>
+  <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+</form>
+
 @if ($errors->any())
   <div class="alert alert-danger">
     <strong>Invalid input. Try again</strong>
@@ -26,20 +38,5 @@
     </ul>
   </div>
 @endif
-
-<form action="{{route('products.store')}}" method="POST">
-  @csrf
-  <div class="row">
-    <div class="col-12">
-      <div class="form-group">
-        <label for="detail">Detail</label>
-        <textarea name="detail" id="detail" cols="30" rows="10"></textarea>
-      </div>
-    </div>
-    <div class="col-12">
-      <button type="submit" class="btn btn-primary btn-lg">Submit</button>
-    </div>
-  </div>
-</form>
 
 @endsection
